3eeabc38f4bafb136e995a49eed5f50fd669dd2a
szymon <szymon1.mirek@student.uj.edu.pl>
2023-05-26 20:09:40 +0200

    Dodane reset do wszystkich id datafixtures. TRUNCATE

--- app/src/DataFixtures/CategoryFixtures.php
+++ app/src/DataFixtures/CategoryFixtures.php
@@ -28,7 +28,12 @@ class CategoryFixtures extends AbstractBaseFixtures
         $connection = $this->manager->getConnection();
         $connection->beginTransaction();
         $connection->exec('SET FOREIGN_KEY_CHECKS = 0');
+        $connection->executeStatement('TRUNCATE TABLE tag');
+        $connection->executeStatement('TRUNCATE TABLE notes_tags');
+        $connection->executeStatement('TRUNCATE TABLE users');
+        $connection->executeStatement('TRUNCATE TABLE todo_item');
         $connection->executeStatement('TRUNCATE TABLE notes');
+        $connection->executeStatement('TRUNCATE TABLE category');
         $connection->exec('SET FOREIGN_KEY_CHECKS = 1');
         //$connection->executeStatement('ALTER TABLE category AUTO_INCREMENT = 1');
 
